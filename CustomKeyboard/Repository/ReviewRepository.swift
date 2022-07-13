aae30194bf428b024faf4b5fb22aaf5332622ab7
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-07-13 13:56:04 +0900

    [#1] feat: 이미지캐싱 구현

--- CustomKeyboard/Repository/ReviewRepository.swift
+++ CustomKeyboard/Repository/ReviewRepository.swift
@@ -9,28 +9,52 @@ import Foundation
 
 final class ReviewRepository {
   
-  let network: NetworkServiceable
+  private let network: NetworkServiceable
+  private var imageCache = NSCache<NSURL,NSData>()
   
   init(network: NetworkService = NetworkService()){
     self.network = network
   }
   
-  func fetchReviews(endPoint: EndPoint, completion: @escaping (Result<Review, Error>) -> Void) {
+  func fetchReviews(endPoint: EndPoint, completion: @escaping (Result<[Review], Error>) -> Void) {
     let URLRequest = URLRequest.makeURLRequest(request: Requset(requestType: .get, endPoint: endPoint))
     network.request(on: URLRequest) { result in
+      var reviews: [Review] = []
       switch result {
       case .success(let data):
         guard let reviewResponse = Decoder<ReviewResponse>().decode(data: data) else {return}
         reviewResponse.data.forEach({
-          print($0.toDomain)
+          reviews.append($0.toDomain())
         })
+        completion(.success(reviews))
       case .failure(let error):
         print(error)
       }
     }
   }
   
+  func loadImage(url:URL, completion: @escaping (Result<Data,Error>) -> Void) {
+    if let data = imageCache.object(forKey: url as NSURL) {
+      completion(.success(data as Data))
+      return
+    }
+    
+    let URLRequest = URLRequest(url: url)
+    network.request(on: URLRequest) { result in
+      switch result{
+      case .success(let data):
+        completion(.success(data))
+      case .failure(let error):
+        print(error)
+      }
+    }
+  }
   
-  
+  func postReviews(endPoint: EndPoint, body: Decodable, completion: @escaping (Bool) -> Void) {
+
+    
+    
+    
+  }
   
 }
